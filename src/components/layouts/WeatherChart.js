7d06c40a0e82d18c08a0942ca71d2255dc20471c
Max Tsh <tatshahdoost@gmail.com>
2020-05-12 18:38:31 +0430

    Added next24Hours without timezone

--- src/components/layouts/WeatherChart.js
+++ src/components/layouts/WeatherChart.js
@@ -1,43 +1,22 @@
 import React from "react";
 import { Bar } from "react-chartjs-2";
 
+// Get The Next 24 Hours From Current Time
+import getDailyHours from "../../utils/getDailyHours";
+
 import "./WeatherChart.css";
 
 function WeatherChart({ data }) {
-  console.log(data);
+  const next24Hours = getDailyHours();
+  console.log(next24Hours);
 
   const hourlyTemp = data.map((each) => each.temp);
 
   const allData = {
-    labels: [
-      1,
-      2,
-      3,
-      4,
-      5,
-      6,
-      7,
-      8,
-      9,
-      10,
-      11,
-      12,
-      13,
-      14,
-      15,
-      16,
-      17,
-      18,
-      19,
-      20,
-      21,
-      22,
-      23,
-      24,
-    ],
+    labels: next24Hours,
     datasets: [
       {
-        label: "Next 24 hours temp °C",
+        label: "Temp °C",
         data: hourlyTemp,
         backgroundColor: "#302E63",
         borderColor: "#302E90",
---
+++ src/utils/getDailyHours.js
@@ -0,0 +1,51 @@
+const time = new Date();
+const nextHours = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  21,
+  22,
+  23,
+  24,
+];
+
+// Todo => Time Zone Issue
+
+export default function getDailyHours() {
+  const next24Hours = nextHours.map((hour) => {
+    // If Current Hour + the next X hour is less than 24 means we can return it without change and we just add PM or AM if it is less or greater than 12
+    if (hour + time.getHours() <= 24) {
+      if (hour + time.getHours() > 12) {
+        return `${hour + time.getHours()} PM`;
+      } else {
+        return `${hour + time.getHours()} AM`;
+      }
+      // If current Hour + the next X hour is greater than 24 means we must reduce 24 hours from it to get the real time inside a 24 hour standard
+    } else {
+      if (hour + time.getHours() - 24 > 12) {
+        return `${hour + time.getHours() - 24} PM`;
+      } else {
+        return `${hour + time.getHours() - 24} AM`;
+      }
+    }
+  });
+
+  return next24Hours;
+}
