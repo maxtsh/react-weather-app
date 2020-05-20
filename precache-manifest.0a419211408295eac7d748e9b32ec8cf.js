554c757ab7523c914d26d8faa919cadc90cc360e
Max Tsh <tatshahdoost@gmail.com>
2020-05-20 16:46:22 +0430

    Updates

---
+++ precache-manifest.0a419211408295eac7d748e9b32ec8cf.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "372ad279b80bac3e0d5d0a4e7a7c8c77",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "c4f6f4965c522dba016a",
+    "url": "/react-weather-app/static/css/main.e0b9187e.chunk.css"
+  },
+  {
+    "revision": "5e59d58aa1d60b3d9980",
+    "url": "/react-weather-app/static/js/0.48c03eec.chunk.js"
+  },
+  {
+    "revision": "e7cad5e8c80a4a756b57f883a8b131b2",
+    "url": "/react-weather-app/static/js/0.48c03eec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7387d5a31226cf6a8cb0",
+    "url": "/react-weather-app/static/js/3.712a0889.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-weather-app/static/js/3.712a0889.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba33035f4bf50a5fcae3",
+    "url": "/react-weather-app/static/js/4.a150edbe.chunk.js"
+  },
+  {
+    "revision": "03c9f70ca48981d384be",
+    "url": "/react-weather-app/static/js/5.ccc0caad.chunk.js"
+  },
+  {
+    "revision": "c4f6f4965c522dba016a",
+    "url": "/react-weather-app/static/js/main.5746059c.chunk.js"
+  },
+  {
+    "revision": "604a48b286a71761019d",
+    "url": "/react-weather-app/static/js/runtime-main.0c17221e.js"
+  },
+  {
+    "revision": "244a401eb43afb0fe739f8dcd8530ae4",
+    "url": "/react-weather-app/static/media/IRANSansWeb.244a401e.ttf"
+  },
+  {
+    "revision": "50f4bba2f37081daeca8c423abdced8b",
+    "url": "/react-weather-app/static/media/IRANSansWeb.50f4bba2.woff"
+  },
+  {
+    "revision": "70a5f97c18c45e8ef620a374be61c6c3",
+    "url": "/react-weather-app/static/media/IRANSansWeb.70a5f97c.eot"
+  },
+  {
+    "revision": "9d38733b8a7b9153f2bf9bfe476ef8e3",
+    "url": "/react-weather-app/static/media/IRANSansWeb.9d38733b.woff2"
+  },
+  {
+    "revision": "465387eeda5050a622ba869f13e53119",
+    "url": "/react-weather-app/static/media/IRANSansWeb_Black.465387ee.eot"
+  },
+  {
+    "revision": "63911f38b2d0bb0650d18555d793dad3",
+    "url": "/react-weather-app/static/media/IRANSansWeb_Black.63911f38.woff2"
+  },
+  {
+    "revision": "7174439308f3d61c0c1f67fd3e052c78",
+    "url": "/react-weather-app/static/media/IRANSansWeb_Black.71744393.ttf"
+  },
+  {
+    "revision": "818f7796aeb3cc761b74334bb0b9aa35",
+    "url": "/react-weather-app/static/media/IRANSansWeb_Black.818f7796.woff"
+  },
+  {
+    "revision": "2f6f164c3e1f1329cc658b3c725bfd98",
+    "url": "/react-weather-app/static/media/IRANSansWeb_Bold.2f6f164c.woff2"
+  },
+  {
+    "revision": "9471804b0f92127716fabbbb2c6647e5",
+    "url": "/react-weather-app/static/media/IRANSansWeb_Bold.9471804b.ttf"
+  },
+  {
+    "revision": "a65cbf91ec2b253357f7621900ef29a4",
+    "url": "/react-weather-app/static/media/IRANSansWeb_Bold.a65cbf91.eot"
+  },
+  {
+    "revision": "d44408d0941f077f60b1306e1331e806",
+    "url": "/react-weather-app/static/media/IRANSansWeb_Bold.d44408d0.woff"
+  },
+  {
+    "revision": "7c207019aa62aa4778add0c2b3372720",
+    "url": "/react-weather-app/static/media/IRANSansWeb_Light.7c207019.woff2"
+  },
+  {
+    "revision": "9d7efb918cab94704068d84361d3f898",
+    "url": "/react-weather-app/static/media/IRANSansWeb_Light.9d7efb91.eot"
+  },
+  {
+    "revision": "aac7a640c059ee7f46c35c2019434117",
+    "url": "/react-weather-app/static/media/IRANSansWeb_Light.aac7a640.woff"
+  },
+  {
+    "revision": "e5b2acaef933a97fa3fb026d8ee73d46",
+    "url": "/react-weather-app/static/media/IRANSansWeb_Light.e5b2acae.ttf"
+  },
+  {
+    "revision": "7bfa046de6ec101d790ba8a54886dc9a",
+    "url": "/react-weather-app/static/media/IRANSansWeb_Medium.7bfa046d.woff"
+  },
+  {
+    "revision": "7c1c84da23760b0d3f59f8de58c47605",
+    "url": "/react-weather-app/static/media/IRANSansWeb_Medium.7c1c84da.woff2"
+  },
+  {
+    "revision": "894895856574b0e65e2739d1a2d1de42",
+    "url": "/react-weather-app/static/media/IRANSansWeb_Medium.89489585.ttf"
+  },
+  {
+    "revision": "c18ca268db923d61b1c9f06415f4c143",
+    "url": "/react-weather-app/static/media/IRANSansWeb_Medium.c18ca268.eot"
+  },
+  {
+    "revision": "2097362beeb263fdd902ad249202cd79",
+    "url": "/react-weather-app/static/media/IRANSansWeb_UltraLight.2097362b.woff"
+  },
+  {
+    "revision": "e0e6c92f90cf30782f81d80a37c22c54",
+    "url": "/react-weather-app/static/media/IRANSansWeb_UltraLight.e0e6c92f.woff2"
+  },
+  {
+    "revision": "eb6bb7948783b20ce01dbb2d103db8df",
+    "url": "/react-weather-app/static/media/IRANSansWeb_UltraLight.eb6bb794.eot"
+  },
+  {
+    "revision": "f8a4f6910724c0373ffd38add5cea6ac",
+    "url": "/react-weather-app/static/media/IRANSansWeb_UltraLight.f8a4f691.ttf"
+  },
+  {
+    "revision": "4ea28db832548be6c1ab50ee8bb9bfe7",
+    "url": "/react-weather-app/static/media/berlin.4ea28db8.jpg"
+  },
+  {
+    "revision": "4a6c0f701537f0d0c965a7c0762fe969",
+    "url": "/react-weather-app/static/media/france.4a6c0f70.jpg"
+  },
+  {
+    "revision": "2b02159fee58d573c079ad5212d56b63",
+    "url": "/react-weather-app/static/media/loader.2b02159f.gif"
+  },
+  {
+    "revision": "90ab8cc069ef03abfc6051c7b6c591ad",
+    "url": "/react-weather-app/static/media/logo.90ab8cc0.png"
+  },
+  {
+    "revision": "c7343e04064e3273d61f47911fb80185",
+    "url": "/react-weather-app/static/media/london.c7343e04.jpg"
+  },
+  {
+    "revision": "fa7cf03db169db7bdd927b6ad4754572",
+    "url": "/react-weather-app/static/media/newyork.fa7cf03d.jpg"
+  },
+  {
+    "revision": "79688925f6dfab47d5a7f9908cb686a9",
+    "url": "/react-weather-app/static/media/seasons.79688925.jpg"
+  },
+  {
+    "revision": "a1d011a06f11b2308ea293de6c807d88",
+    "url": "/react-weather-app/static/media/weather-draw.a1d011a0.svg"
+  }
+]);
