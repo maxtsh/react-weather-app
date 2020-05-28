41896b6578c2264a2bca67fc68f898e47119a02e
Max Tsh <tatshahdoost@gmail.com>
2020-05-28 14:55:00 +0430

    Updates

---
+++ precache-manifest.c89e5d1769f1a5433a1ead1a56af9c75.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "140b84f7590a8af621b55c0bb0a2b9c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "febf4c73668c4a743a2a",
+    "url": "/static/css/main.7b635573.chunk.css"
+  },
+  {
+    "revision": "15ac68c1783207b5e598",
+    "url": "/static/js/0.a4190db1.chunk.js"
+  },
+  {
+    "revision": "e7cad5e8c80a4a756b57f883a8b131b2",
+    "url": "/static/js/0.a4190db1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0b9b183b68d306d6bb4",
+    "url": "/static/js/3.e0f9c682.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/3.e0f9c682.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3157c3f6be166671757d",
+    "url": "/static/js/4.7ed9055c.chunk.js"
+  },
+  {
+    "revision": "dfcd0ab8c6d57e17d6d2",
+    "url": "/static/js/5.6cd9e9c1.chunk.js"
+  },
+  {
+    "revision": "febf4c73668c4a743a2a",
+    "url": "/static/js/main.5c84dc0b.chunk.js"
+  },
+  {
+    "revision": "32d69253c20f5b4ccad5",
+    "url": "/static/js/runtime-main.0db47012.js"
+  },
+  {
+    "revision": "244a401eb43afb0fe739f8dcd8530ae4",
+    "url": "/static/media/IRANSansWeb.244a401e.ttf"
+  },
+  {
+    "revision": "50f4bba2f37081daeca8c423abdced8b",
+    "url": "/static/media/IRANSansWeb.50f4bba2.woff"
+  },
+  {
+    "revision": "70a5f97c18c45e8ef620a374be61c6c3",
+    "url": "/static/media/IRANSansWeb.70a5f97c.eot"
+  },
+  {
+    "revision": "9d38733b8a7b9153f2bf9bfe476ef8e3",
+    "url": "/static/media/IRANSansWeb.9d38733b.woff2"
+  },
+  {
+    "revision": "465387eeda5050a622ba869f13e53119",
+    "url": "/static/media/IRANSansWeb_Black.465387ee.eot"
+  },
+  {
+    "revision": "63911f38b2d0bb0650d18555d793dad3",
+    "url": "/static/media/IRANSansWeb_Black.63911f38.woff2"
+  },
+  {
+    "revision": "7174439308f3d61c0c1f67fd3e052c78",
+    "url": "/static/media/IRANSansWeb_Black.71744393.ttf"
+  },
+  {
+    "revision": "818f7796aeb3cc761b74334bb0b9aa35",
+    "url": "/static/media/IRANSansWeb_Black.818f7796.woff"
+  },
+  {
+    "revision": "2f6f164c3e1f1329cc658b3c725bfd98",
+    "url": "/static/media/IRANSansWeb_Bold.2f6f164c.woff2"
+  },
+  {
+    "revision": "9471804b0f92127716fabbbb2c6647e5",
+    "url": "/static/media/IRANSansWeb_Bold.9471804b.ttf"
+  },
+  {
+    "revision": "a65cbf91ec2b253357f7621900ef29a4",
+    "url": "/static/media/IRANSansWeb_Bold.a65cbf91.eot"
+  },
+  {
+    "revision": "d44408d0941f077f60b1306e1331e806",
+    "url": "/static/media/IRANSansWeb_Bold.d44408d0.woff"
+  },
+  {
+    "revision": "7c207019aa62aa4778add0c2b3372720",
+    "url": "/static/media/IRANSansWeb_Light.7c207019.woff2"
+  },
+  {
+    "revision": "9d7efb918cab94704068d84361d3f898",
+    "url": "/static/media/IRANSansWeb_Light.9d7efb91.eot"
+  },
+  {
+    "revision": "aac7a640c059ee7f46c35c2019434117",
+    "url": "/static/media/IRANSansWeb_Light.aac7a640.woff"
+  },
+  {
+    "revision": "e5b2acaef933a97fa3fb026d8ee73d46",
+    "url": "/static/media/IRANSansWeb_Light.e5b2acae.ttf"
+  },
+  {
+    "revision": "7bfa046de6ec101d790ba8a54886dc9a",
+    "url": "/static/media/IRANSansWeb_Medium.7bfa046d.woff"
+  },
+  {
+    "revision": "7c1c84da23760b0d3f59f8de58c47605",
+    "url": "/static/media/IRANSansWeb_Medium.7c1c84da.woff2"
+  },
+  {
+    "revision": "894895856574b0e65e2739d1a2d1de42",
+    "url": "/static/media/IRANSansWeb_Medium.89489585.ttf"
+  },
+  {
+    "revision": "c18ca268db923d61b1c9f06415f4c143",
+    "url": "/static/media/IRANSansWeb_Medium.c18ca268.eot"
+  },
+  {
+    "revision": "2097362beeb263fdd902ad249202cd79",
+    "url": "/static/media/IRANSansWeb_UltraLight.2097362b.woff"
+  },
+  {
+    "revision": "e0e6c92f90cf30782f81d80a37c22c54",
+    "url": "/static/media/IRANSansWeb_UltraLight.e0e6c92f.woff2"
+  },
+  {
+    "revision": "eb6bb7948783b20ce01dbb2d103db8df",
+    "url": "/static/media/IRANSansWeb_UltraLight.eb6bb794.eot"
+  },
+  {
+    "revision": "f8a4f6910724c0373ffd38add5cea6ac",
+    "url": "/static/media/IRANSansWeb_UltraLight.f8a4f691.ttf"
+  },
+  {
+    "revision": "4ea28db832548be6c1ab50ee8bb9bfe7",
+    "url": "/static/media/berlin.4ea28db8.jpg"
+  },
+  {
+    "revision": "4a6c0f701537f0d0c965a7c0762fe969",
+    "url": "/static/media/france.4a6c0f70.jpg"
+  },
+  {
+    "revision": "2b02159fee58d573c079ad5212d56b63",
+    "url": "/static/media/loader.2b02159f.gif"
+  },
+  {
+    "revision": "90ab8cc069ef03abfc6051c7b6c591ad",
+    "url": "/static/media/logo.90ab8cc0.png"
+  },
+  {
+    "revision": "c7343e04064e3273d61f47911fb80185",
+    "url": "/static/media/london.c7343e04.jpg"
+  },
+  {
+    "revision": "fa7cf03db169db7bdd927b6ad4754572",
+    "url": "/static/media/newyork.fa7cf03d.jpg"
+  },
+  {
+    "revision": "79688925f6dfab47d5a7f9908cb686a9",
+    "url": "/static/media/seasons.79688925.jpg"
+  },
+  {
+    "revision": "a1d011a06f11b2308ea293de6c807d88",
+    "url": "/static/media/weather-draw.a1d011a0.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-weather-app/precache-manifest.5e936297c0b67b450a3e7a4c734c5de3.js"
+  "/precache-manifest.c89e5d1769f1a5433a1ead1a56af9c75.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-weather-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
